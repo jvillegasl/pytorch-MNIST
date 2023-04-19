ff4477f7bb738fcf9f6ccafcc4d14b2fa19bee3e
jvillegasl <jvillegasl>
2023-04-19 17:59:29 -0500

    fix: removed unused imports

--- train.py
+++ train.py
@@ -1,27 +1,26 @@
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.optim import Adam
-from torch.utils.data.dataloader import DataLoader
 from base import BaseModel
-
+from config import HYPER
 from data_loader import MNISTDataLoader
-from model import accuracy
 from trainer import Trainer
 
-def train(model: BaseModel):
-    batch_size = 100
-    num_epochs = 10
-    loss_fn = F.cross_entropy
-    lr = 0.005
-    opt_fn = Adam
-    metric = accuracy
 
-    mnist = MNISTDataLoader(batch_size=100)
+def train(model: BaseModel, save_model: bool = True):
+    batch_size = HYPER['batch_size']
+    num_epochs = HYPER['num_epochs']
+    loss_fn = HYPER['loss_fn']
+    lr = HYPER['lr']
+    opt_fn = HYPER['opt_fn']
+    metric = HYPER['metric']
+
+    mnist = MNISTDataLoader(batch_size)
     train_dl = mnist.train_dl
     val_dl = mnist.val_dl
 
-    trainer = Trainer(model, num_epochs, loss_fn, train_dl, val_dl, lr, opt_fn, metric)
+    trainer = Trainer(model, num_epochs, loss_fn,
+                      train_dl, val_dl, lr, opt_fn, metric)
     history = trainer.fit()
-    model.save()
+
+    if save_model:
+        model.save()
     
-    pass
+    return history
