afe17e636a37469593a451655b2088f142f38271
jvillegasl <jvillegasl>
2023-04-19 17:59:54 -0500

    update: added onnx text

--- test.py
+++ test.py
@@ -1,24 +1,20 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch import Tensor
-from torch.optim import Adam
-from torch.utils.data.dataloader import DataLoader
-from typing import List, Type
+import onnxruntime
 import matplotlib.pyplot as plt
+from typing import List
 
-from base import BaseModel
+from config import HYPER
 from data_loader import MNISTDataLoader
-from model import accuracy, evaluate,MNISTModel
+from model import evaluate
+from utils import image_to_tensor
 
 
-def test(model: BaseModel):
-    batch_size = 100
-    num_epochs = 10
-    loss_fn = F.cross_entropy
-    lr = 0.005
-    opt_fn = Adam
-    metric = accuracy
+def test(model: nn.Module):
+    batch_size = HYPER['batch_size']
+    loss_fn = HYPER['loss_fn']
+    metric = HYPER['metric']
 
     mnist = MNISTDataLoader(batch_size)
     test_dl = mnist.test_dl
@@ -27,24 +23,29 @@ def test(model: BaseModel):
 
     print('Loss: {:.4f}, Accuracy: {:.4f}'.format(test_loss, test_acc))
 
-def predict_image(model: BaseModel, image: Tensor, label_index: int, classes: List[str]):
+
+def predict_image(model: nn.Module, image: Tensor, label_index: int, classes: List[str]):
     xb = image.unsqueeze(0)
 
     yb: Tensor = model(xb)
+    print(yb)
 
     preds: Tensor
     _, preds = torch.max(yb, dim=1)
 
     pred_index = int(preds[0].item())
 
-    print('Label: {}, Predicted: {}'.format(classes[label_index], classes[pred_index]))
-    plt.imshow(image.permute(1,2,0).cpu())
-    plt.title('Label: {}, Predicted: {}'.format(classes[label_index], classes[pred_index]))
+    print('Label: {}, Predicted: {}'.format(
+        classes[label_index], classes[pred_index]))
+    plt.imshow(image.permute(1, 2, 0).cpu())
+    plt.title('Label: {}, Predicted: {}'.format(
+        classes[label_index], classes[pred_index]))
     plt.show()
 
     return classes[pred_index]
 
-def predict_random_image(model: BaseModel):
+
+def predict_random_image(model: nn.Module):
     mnist = MNISTDataLoader(100)
     classes = mnist.dataset.classes
 
@@ -56,11 +57,20 @@ def predict_random_image(model: BaseModel):
 
         image = xb[0]
         label_index = int(yb[0].item())
-
+        print(image)
         predict_image(model, image, label_index, classes)
         break
 
-if __name__ == '__main__':
-    model = MNISTModel()
-    
-    predict_random_image(model)
+
+def test_onnx(model_path: str, image_path: str):
+    session = onnxruntime.InferenceSession(model_path)
+
+    def to_numpy(tensor: Tensor):
+        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+    img = image_to_tensor(image_path, invert_colors=True)
+    inputs = {session.get_inputs()[0].name: to_numpy(img.unsqueeze(0))}
+    outputs = session.run(None, inputs)
+    y = list(outputs[0])
+
+    return y
